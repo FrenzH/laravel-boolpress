1a21c5b0f036a80818136fb80904ff20fc74ae39
FrenzH <franciattom99@gmail.com>
2022-11-21 15:28:31 +0000

    get guest home using vue

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+Route::get('/', function(){
+    return view("guest.home");
+})->name('index');
+
+
+Route::get("{any?}", function() {
+    return view("guest.home");
+})->where("any", ".*");
